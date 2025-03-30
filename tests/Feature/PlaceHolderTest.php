59d2b286a2a479bd9361358ccbc7f6eb8fb8d05e
Jerome Thayananthajothy <jerome.thavarshan@brompton.co.uk>
2025-03-30 23:53:40 +0530

    Add initial placeholder test for Cerberus feature

---
+++ tests/Feature/PlaceHolderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cerberus\Tests\Feature;
+
+use Cerberus\Tests\TestCase;
+
+class PlaceHolderTest extends TestCase
+{
+    public function test_place_holder()
+    {
+        $this->assertTrue(true);
+    }
+}
