1468ab43bf7abdef5abcf34891ebf76eec91cd1c
Jerome Thayananthajothy <jerome.thavarshan@brompton.co.uk>
2025-04-11 06:40:56 +0530

    Add onboarding method to Organisation resource
    - Implemented the `onboard` method to handle the onboarding process for new organisations.
    - The method accepts an array of data and sends a POST request to the '/onboarding' endpoint with the organisation and owner details.
    - The method returns a boolean indicating the success of the operation.

--- src/Cerberus/Resources/Organisation.php
+++ src/Cerberus/Resources/Organisation.php
@@ -27,4 +27,32 @@ class Organisation extends Resource
         'updated_at',
         'deleted_at',
     ];
+
+    /**
+     * The attributes that are hidden from the response.
+     *
+     * @var array<int, string>
+     */
+    public static function onboard(array $data): bool
+    {
+        return self::newInstance()
+            ->getConnection()
+            ->post('/onboarding', [
+                'organisation' => [
+                    'name' => $data['organisation'],
+                    'email' => $data['email'],
+                    'phone' => $data['phone'],
+                ],
+                'owner' => [
+                    'first_name' => $data['first_name'],
+                    'last_name' => $data['last_name'],
+                    'username' => $data['username'],
+                    'email' => $data['email'],
+                    'phone' => $data['phone'],
+                    'password' => $data['password'],
+                    'password_confirmation' => $data['password_confirmation'],
+                ],
+            ])
+            ->ok();
+    }
 }
