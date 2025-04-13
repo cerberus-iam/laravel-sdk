638a109ced2f2aa76a8eec66299d416ea095a77d
Jerome Thayananthajothy <jerome.thavarshan@brompton.co.uk>
2025-04-13 09:20:58 +0530

    Implement revoke method in Client class to handle client revocation by ID or instance

--- src/Cerberus/Resources/Client.php
+++ src/Cerberus/Resources/Client.php
@@ -2,10 +2,32 @@
 
 namespace Cerberus\Resources;
 
+use InvalidArgumentException;
+
 class Client extends Resource
 {
     /**
      * Name of the resource.
      */
     public string $resource = 'clients';
+
+    /**
+     * Revoke a client by its ID or the current instance.
+     *
+     * @throws InvalidArgumentException If no valid client can be found to revoke.
+     */
+    public function revoke(?string $id = null): bool
+    {
+        $client = $id ? static::find($id) : $this;
+
+        if (! $client || ! $client->exists) {
+            throw new InvalidArgumentException(
+                $id
+                    ? "Client with ID {$id} not found or does not exist."
+                    : 'Client ID is required to revoke a client.'
+            );
+        }
+
+        return $client->update(['revoked' => true]);
+    }
 }
