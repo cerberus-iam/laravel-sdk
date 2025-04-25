5956c84462d507d9aab6b8c4047fa0e1e6f4b748
Jerome Thayananthajothy <jerome.thavarshan@brompton.co.uk>
2025-04-26 04:11:36 +0530

    feat: Define fillable attributes for the Activity resource

--- src/Cerberus/Resources/Activity.php
+++ src/Cerberus/Resources/Activity.php
@@ -8,4 +8,20 @@ class Activity extends Resource
      * Name of the resource.
      */
     public string $resource = 'activities';
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'organisation_id',
+        'user_id',
+        'type',
+        'changes',
+        'trackable_type',
+        'trackable_id',
+        'started_at',
+        'ended_at',
+    ];
 }
